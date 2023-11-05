ce7f34ed253224d22b56b490fa7ca9d23846e65f
shathi <shathi@gmail.com>
2023-11-06 02:19:08 +0600

    setup project and get category data and also add blog

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.POST || 5000
+require('dotenv').config()
+app.use(express.json())
+app.use(cors())
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PSS}@cluster0.zoyeiku.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const CategoryshopCollection = client.db("BlogDB").collection("category");
+        const BlogCollection = client.db("BlogDB").collection("blogs");
+
+
+        // category section
+        app.get('/category', async(req, res) =>{
+            const cursor = CategoryshopCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // blogs section 
+
+        app.post('/add-blog', async(req, res) =>{
+            const blogData = req.body
+            const result = await BlogCollection.insertOne(blogData)
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => {
+    console.log('My Server Runing On Port', port)
+})
