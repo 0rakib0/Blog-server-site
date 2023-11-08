4045e95c8c6ae6df374ca6d031207c08482a9833
rakib <hassanrakibul926@gmail.com>
2023-11-08 13:40:08 +0600

    get recent comment

--- index.js
+++ index.js
@@ -180,6 +180,11 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/comment', async(req, res) =>{
+            const result = await CommentCOllection.find().limit(5).toArray()
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
